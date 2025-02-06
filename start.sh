89e8dc8d7d3d587b0eb36beb522d46b56aab54eb
tionis <me@tionis.dev>
2025-02-06 19:17:45 +0100

    small fix for auto restore

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ if ! test -d /data; then
 	mkdir /data
 fi
 if ! test -f /data/denokv.sqlite3; then
-	litestream restore /data/denokv.sqlite3
+	litestream restore -if-replica-exists -v /data/denokv.sqlite3
 fi
 # acquite some distributed lock here?
 litestream replicate -exec '/usr/local/bin/denokv serve'
